8ae13cb2a425d7e299eb7d1f19593f9e17832093
SS1857 <89942518+SS1857@users.noreply.github.com>
2022-05-23 04:49:32 +0530

    Add files via upload

--- GL-Java-FSD-DB-LAB/08-Question-Work.sql
+++ GL-Java-FSD-DB-LAB/08-Question-Work.sql
@@ -1,9 +1,6 @@
 # Q8) Display customer name and gender whose names start or 
 # end with character 'A'
 
-select * from customer where cus_name Like '%A';
-
-select * from customer where (cus_name Like 'A%' or cus_name like '%A');
-
-select * from customer where (cus_name Like 'A%A')
+select customer.cus_name,customer.cus_gender from customer 
+where customer.cus_name like 'A%' or customer.cus_name like '%A';
 
